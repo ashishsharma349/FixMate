151cb9fbeff64f73330b46dc1f8746fb8ed33e0b
Ashish Sharma <ashishsharma90807@gmail.com>
2026-05-24 13:47:44 +0530

    fix(auth): secure change-password route with verifyToken

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const authRouter = express.Router();
 const authController = require('../controller/auth.js');
+const { verifyToken } = require('../middleware/jwtMiddleware');
 
 authRouter.post('/login', express.json(), authController.handlePost_login);
 authRouter.post('/logout', authController.handle_logout);
 
 // Change password (first login forced change, or voluntary)
-authRouter.post('/change-password', express.json(), authController.handlePost_changePassword);
+authRouter.post('/change-password', verifyToken, express.json(), authController.handlePost_changePassword);
 
 module.exports = authRouter;
