7758d07d12ee92379298531f40aa32c60c239612
Ashish Sharma <ashishsharma90807@gmail.com>
2026-05-25 14:15:00 +0530

    feat(admin-crud): create user and staff repositories

---
+++ repositories/StaffRepository.js
@@ -0,0 +1,51 @@
+const Staff = require("../model/staff");
+const Auth = require("../model/Auth");
+
+class StaffRepository {
+  // Find all staff populated with auth credentials
+  async findAll() {
+    return await Staff.find().populate("authId", "email role isFirstLogin");
+  }
+
+  // Find auth record by email
+  async findByEmail(email) {
+    return await Auth.findOne({ email });
+  }
+
+  // Create authentication credentials using a database session
+  async createAuth(data, session) {
+    return await Auth.create([data], { session }).then(res => res[0]);
+  }
+
+  // Create staff profile using a database session
+  async createStaff(data, session) {
+    return await Staff.create([data], { session }).then(res => res[0]);
+  }
+
+  // Find staff profile by ID
+  async findById(id) {
+    return await Staff.findById(id);
+  }
+
+  // Update staff profile by ID
+  async update(id, updateData) {
+    return await Staff.findByIdAndUpdate(id, { $set: updateData }, { new: true });
+  }
+
+  // Update authentication credentials by ID
+  async updateAuth(authId, updateData) {
+    return await Auth.findByIdAndUpdate(authId, { $set: updateData }, { new: true });
+  }
+
+  // Delete staff profile by ID
+  async delete(id) {
+    return await Staff.findByIdAndDelete(id);
+  }
+
+  // Delete authentication credentials by ID
+  async deleteAuth(authId) {
+    return await Auth.findByIdAndDelete(authId);
+  }
+}
+
+module.exports = new StaffRepository();
